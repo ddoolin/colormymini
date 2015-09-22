8ce62c4647e0c570c87fe3946aac39217e91b8ae
Cole Gentry <cole.gentry@sungard.com>
2015-09-22 13:54:28 -0400

    converts naming convention to match colors instead of banks; puts together a giant list of current color values;

---
+++ js/app.js
@@ -0,0 +1,40 @@
+(function(){
+  var app = angular.module('colorSearch', ['ngMaterial']);
+
+  app.factory('ColorService', ['$q', '$http', function ($q, $http) {
+    var deferred = $q.defer();
+
+    $http.get('/js/colors.json')
+      .success(function (colors) {
+        deferred.resolve(colors);
+      })
+      .error(function (err) {
+        console.log('Error getting colors.', err);
+        deferred.reject(err);
+      });
+
+    return deferred.promise;
+  }]);
+
+  app.controller('ColorCtrl', ['$scope', 'ColorService', function ($scope, ColorService) {
+    var self = $scope;
+
+    self.filterBanks = function(searchText){
+      searchText = searchText ? searchText.toLowerCase() : '';
+      return self.colors.filter(function (color) {
+        var colorName = color.name.toLowerCase();
+        return colorName.indexOf(searchText) !== -1;
+      });
+    };
+
+    self.selectBank = function(color){
+      // console.log('SelectBank', color);
+    };
+
+    ColorService.then(function (colors) {
+      self.colors = colors;
+    }, function (err) {
+      // TODO: Error, handle it
+    });
+  }]);
+})();
